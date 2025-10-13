4a3a8b84169caf76dd0e14b2bed2e30aad5e6984
Felipe <lamediatricolor@gmail.com>
2025-10-13 16:54:49 -0500

    String update

--- Java Master/p01_basicos/src/main/java/com/cultodeportivo/strings/EjemploString.java
+++ Java Master/p01_basicos/src/main/java/com/cultodeportivo/strings/EjemploString.java
@@ -1,5 +1,12 @@
 package com.cultodeportivo.strings;
 
 public class EjemploString {
-    
+
+    public static void main(String[] args) {
+        String curso = "Master Java";
+        System.out.println("String: " + curso);
+
+        boolean esIgual = curso.equals("Master Java");
+        System.out.println("Es igual: " + esIgual);
+    }
 }
